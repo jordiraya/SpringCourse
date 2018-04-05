2b5f7a123c9f36599108b2bc54a91c3be08bd119
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-04-05 13:19:07 +0200

    Lecture 46

--- spring-mvc-demo-1/src/com/timbuchalka/springdemo/controller/WelcomeController.java
+++ spring-mvc-demo-1/src/com/timbuchalka/springdemo/controller/WelcomeController.java
@@ -2,27 +2,33 @@ package com.timbuchalka.springdemo.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.timbuchalka.service.demo.GenericWelcomeService;
 import com.timbuchalka.service.demo.WelcomeService;
 
 @Controller
 public class WelcomeController {
+	
+	@Autowired
+	private GenericWelcomeService welcomeService;
 	
 	@RequestMapping("/")
 	// http://localhost:8080/spring
 	public String doWelcome(Model model) {
 		
 		// 1. Retrieving the processed data
-		WelcomeService welcomeService = new WelcomeService();
+		// WelcomeService welcomeService = new WelcomeService();
 		List<String> welcomeMessage = welcomeService.getWelcomeMessage("Jordi");
 		
 		// 2. Add data to the model
 		model.addAttribute("myWelcomeMessage", welcomeMessage);
 		
-		return "weolcomeNew"; // name of the view (jsp)
+		// 3. Return logical view name
+		return "welcomeNew"; // viewResolver resolves to WEB-INF/views/welcomeNew.jsp
 	}
 
 }
