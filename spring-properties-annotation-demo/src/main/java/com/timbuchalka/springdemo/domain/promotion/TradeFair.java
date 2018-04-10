13396dafebca27d0060e280c5ad3060d21c146ad
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-04-10 15:18:22 +0200

    Lecture 64: handling properties in Spring: pure Annotations

---
+++ spring-properties-annotation-demo/src/main/java/com/timbuchalka/springdemo/domain/promotion/TradeFair.java
@@ -0,0 +1,33 @@
+package com.timbuchalka.springdemo.domain.promotion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.timbuchalka.springdemo.domain.product.ConsumerProduct;
+import com.timbuchalka.springdemo.domain.product.IndustrialProduct;
+
+@Component("myfair")
+public class TradeFair {
+
+	@Autowired
+	private IndustrialProduct industrialProduct;
+	
+	@Autowired
+	private ConsumerProduct consumerProduct;
+	
+
+	public int declareIndustrialProductPrice(IndustrialProduct industrialProduct) {
+		return industrialProduct.calculatePrice();
+	} 
+
+	public int declareConsumerProductPrice(ConsumerProduct consumerProduct) {
+		return consumerProduct.calculatePrice();
+	} 
+	
+	public String specialPromotionalPricing() {
+		String specialPriceInfo = "IndustrialProduct is priced at $" + declareIndustrialProductPrice(industrialProduct) + " and " +
+		"ConsumerProduct is priced at $" + declareConsumerProductPrice(consumerProduct);
+		return specialPriceInfo;
+	}
+	
+}
