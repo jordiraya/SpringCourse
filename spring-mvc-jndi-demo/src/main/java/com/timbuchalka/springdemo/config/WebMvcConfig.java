84c5fee546e7d4b403cbfb97d7f99c9c662906c3
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-04-16 17:32:26 +0200

    Lecture 87: JNDI Datasource

--- spring-mvc-jndi-demo/src/main/java/com/timbuchalka/springdemo/config/WebMvcConfig.java
+++ spring-mvc-jndi-demo/src/main/java/com/timbuchalka/springdemo/config/WebMvcConfig.java
@@ -1,8 +1,11 @@
 package com.timbuchalka.springdemo.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.JstlView;
@@ -15,6 +18,17 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+	@Bean
+	public DataSource dataSource() {
+		final JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
+		dsLookup.setResourceRef(true);
+		DataSource dataSource = dsLookup.getDataSource("jdbc/springdb");
+		// alternative
+		// dsLookup.setResourceRef(false);
+		// DataSource dataSource = dsLookup.getDataSource("java:comp/env/jdbc/springdb");
+		return dataSource;
+	}
+	
 	@Bean
 	public UrlBasedViewResolver urlBasedViewResolver() {
 		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
